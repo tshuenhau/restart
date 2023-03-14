a4ec3df28641f45fb72ea43fb2822651ea6a6a47
tshuenhau <tshuenhau@gmail.com>
2023-03-15 02:03:04 +0800

    EditProfilePage

---
+++ lib/widgets/EditProfileField.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class EditProfileField extends StatefulWidget {
+  EditProfileField({required this.field, Key? key}) : super(key: key);
+
+  String field;
+
+  @override
+  State<EditProfileField> createState() => _EditProfileFieldState();
+}
+
+class _EditProfileFieldState extends State<EditProfileField> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: MediaQuery.of(context).size.width * 70 / 100,
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Text(
+                widget.field,
+                textAlign: TextAlign.start,
+              ),
+            ),
+            TextFormField(
+              textAlign: TextAlign.start,
+              keyboardType: TextInputType.name,
+              initialValue: widget.field,
+              onChanged: (val) {
+                if (val.length > 0) {
+                  setState(() {
+                    widget.field = val;
+                  });
+                }
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Cannot be empty';
+                }
+                return null;
+                // doSubmit();
+              },
+            )
+          ],
+        ));
+  }
+}
