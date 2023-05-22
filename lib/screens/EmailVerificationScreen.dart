2e2125eacbe78025536420943861480a30b757d4
tshuenhau <tshuenhau@gmail.com>
2023-05-22 14:20:41 +0800

    styled email verification page

--- lib/screens/EmailVerificationScreen.dart
+++ lib/screens/EmailVerificationScreen.dart
@@ -1,13 +1,41 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:restart/widgets/GlassCards/GlassCard.dart';
+import 'package:restart/widgets/GlassCards/GlassCard_header.dart';
+import 'package:restart/widgets/Glasscards/Header.dart';
+import 'package:restart/widgets/layout/CustomScaffold.dart';
 
 class EmailVerificationScreen extends StatelessWidget {
   const EmailVerificationScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(child: Text("Verify your email!"));
+    return CustomScaffold(
+        body: SingleChildScrollView(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 1.5 / 100,
+                bottom: MediaQuery.of(context).size.height * 3 / 100),
+            child: GlassCard_header(
+              header: Header(title: "Email Verification"),
+              height: MediaQuery.of(context).size.height * 80 / 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AutoSizeText(
+                      "Please verify you email to complete your registration.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height * 1.5 / 100),
+                  ElevatedButton(
+                      onPressed: () {}, child: AutoSizeText("Try Again"))
+                ],
+              ),
+            )));
   }
 }
