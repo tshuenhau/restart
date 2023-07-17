80e3593a8fb233864d4c54d0b6e0c016cafdfb89
ChuangZheQuan <zhequan7chuang@gmail.com>
2023-07-17 16:59:19 +0800

    change env

--- lib/env.dart
+++ lib/env.dart
@@ -1,7 +1,9 @@
 // const API_URL = "https://re-start-backend.onrender.com";
 // const API_URL =
 //     "http://restart-backend-env-1.eba-cwdqrawq.ap-southeast-2.elasticbeanstalk.com";
-const API_URL = "http://192.168.68.52:3000";
+// const API_URL = "http://192.168.68.52:3000";
+const API_URL =
+    "http://restart-backend-test-env.eba-wqcpb9ag.ap-southeast-2.elasticbeanstalk.com/";
 const GOOGLE_MAPS_API_KEY = "AIzaSyAIeHmnynotSQckgE9-yjTMZQFWzC-hmAw";
 const TIMESLOTS_API_URL = "http://192.168.68.52:3001";
 // const TIMESLOTS_API_URL =
